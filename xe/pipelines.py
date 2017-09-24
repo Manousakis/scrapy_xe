7a0077d014990017f20bd849b6f16970b299f2e4
Manousakis <Manousakis@users.noreply.github.com>
2017-09-24 18:43:01 +0300

    Revert "Test line removed"
    This reverts commit a37fc18dca5fd685d14fe6b401cb33521dd8582b.

--- xe/pipelines.py
+++ xe/pipelines.py
@@ -11,6 +11,7 @@ import smtplib
 class XePipeline(object):
 
     def close_spider(self, spider):
+<<<<<<< HEAD
         mail = smtplib.SMTP('smtp.gmail.com',587)
         mail.ehlo()
         mail.starttls()
@@ -21,3 +22,20 @@ class XePipeline(object):
         mail_content = "Spider "+spider.name+" closed"
         mail.sendmail(emailAddress,reciever,mail_content)
         mail.close()
+=======
+        directory = u"E:/Documents/OneDrive/4_Προγραμματισμός/Scrapy/Αγορά Ακινήτων/"+spider.name+u"/Αρχεία_pd.DataFrame"
+        os.chdir(directory)
+        now = datetime.now()
+        filename = spider.name+" "+str(now.year)+"-"+str(now.month)+"-"+str(now.day)+" "+'Items.pickle'
+        with open(filename, 'w') as file:
+            pickle.dump(self.df,file)
+        # Check for empty fields
+        colList = self.df.columns.tolist()
+        for col in colList:
+            if self.df[col].isnull().all():
+                logger.warning("Found Null Field, in file " + filename +": " + col)
+        # This line is a test
+
+    def process_item(self, item, spider):
+        self.df = self.df.append(item,ignore_index=True)
+>>>>>>> parent of a37fc18... Test line removed

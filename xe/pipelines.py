d08a92daa125020e4da6e2126a65e9e2a3eee202
Manousakis <Manousakis@users.noreply.github.com>
2017-09-24 18:43:40 +0300

    kjkljlk

--- xe/pipelines.py
+++ xe/pipelines.py
@@ -11,18 +11,6 @@ import smtplib
 class XePipeline(object):
 
     def close_spider(self, spider):
-<<<<<<< HEAD
-        mail = smtplib.SMTP('smtp.gmail.com',587)
-        mail.ehlo()
-        mail.starttls()
-        emailAddress = 'ivmpythonscripts@gmail.com'
-        pwd = 'Gh74d8G5GDJU6ds3'
-        mail.login(emailAddress,pwd)
-        reciever = 'vouvakismanousakis@outlook.com'
-        mail_content = "Spider "+spider.name+" closed"
-        mail.sendmail(emailAddress,reciever,mail_content)
-        mail.close()
-=======
         directory = u"E:/Documents/OneDrive/4_Προγραμματισμός/Scrapy/Αγορά Ακινήτων/"+spider.name+u"/Αρχεία_pd.DataFrame"
         os.chdir(directory)
         now = datetime.now()
@@ -38,4 +26,3 @@ class XePipeline(object):
 
     def process_item(self, item, spider):
         self.df = self.df.append(item,ignore_index=True)
->>>>>>> parent of a37fc18... Test line removed
